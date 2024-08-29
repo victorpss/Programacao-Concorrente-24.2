eb8fe42008842145a66cd80e7e1a641f6af9fb3a
victorpss <victorpss@dcc.ufrj.br>
2024-08-28 23:34:16 -0300

    iniciado lab3

---
+++ Lab3/geraMatrizBinario.c
@@ -0,0 +1,105 @@
+/* Programa que cria arquivo com uma matriz de valores do tipo float, gerados aleatoriamente 
+ * Entrada: dimensoes da matriz (n e m) e nome do arquivo de saida
+ * Saida: arquivo binario com as dimensoes (valores inteiros) da matriz (n e m), 
+ * seguido dos valores (float) de todas as celulas da matriz gerados aleatoriamente
+ * */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+//descomentar o define abaixo caso deseje imprimir uma versao truncada da matriz gerada no formato texto
+#define TEXTO 
+
+int main(int argc, char*argv[]) {
+   float *matriz1, *matriz2; //matriz que será gerada
+   int n, m; //dimensoes da matriz
+   long long int tam; //qtde de elementos na matriz
+   FILE * descritorArquivo; //descritor do arquivo de saida
+   size_t ret; //retorno da funcao de escrita no arquivo de saida
+   
+   //recebe os argumentos de entrada
+   if(argc < 4) {
+      fprintf(stderr, "Digite: %s <linhas> <colunas> <arquivo saida>\n", argv[0]);
+      return 1;
+   }
+   n = atoi(argv[1]); 
+   m = atoi(argv[2]);
+   tam = n * m;
+
+   //aloca memoria para a primeira matriz
+   matriz1 = (float*) malloc(sizeof(float) * tam);
+   if(!matriz1) {
+      fprintf(stderr, "Erro de alocao da memoria da matriz 1\n");
+      return 2;
+   }
+
+   //preenche a primeira matriz com valores float aleatorios
+   //randomiza a sequencia de numeros aleatorios
+   srand(time(NULL));
+   for(long int i=0; i<tam; i++) {
+       *(matriz1+i) = (rand() % 1000) * 0.3;
+   }
+
+   //imprimir na saida padrao a matriz gerada
+   #ifdef TEXTO
+   for(int i=0; i<n; i++) {
+      for(int j=0; j<m; j++)
+        fprintf(stdout, "%.6f ", matriz1[i*m+j]);
+      fprintf(stdout, "\n");
+   }
+   #endif
+
+   //aloca memoria para a segunda matriz
+   matriz2 = (float*) malloc(sizeof(float) * tam);
+   if(!matriz2) {
+      fprintf(stderr, "Erro de alocao da memoria da matriz 2\n");
+      return 2;
+   }
+
+   //preenche a segunda matriz com valores float aleatorios
+   //randomiza a sequencia de numeros aleatorios
+   for(long int i=0; i<tam; i++) {
+       *(matriz2+i) = (rand() % 1000) * 0.3;
+   }
+
+   //escreve a matriz no arquivo
+   //abre o arquivo para escrita binaria
+   descritorArquivo = fopen(argv[3], "wb");
+   if(!descritorArquivo) {
+      fprintf(stderr, "Erro de abertura do arquivo\n");
+      return 3;
+   }
+   //escreve numero de n e de m
+   ret = fwrite(&n, sizeof(int), 1, descritorArquivo);
+   ret = fwrite(&m, sizeof(int), 1, descritorArquivo);
+   //escreve os elementos da primeira matriz
+   ret = fwrite(matriz1, sizeof(float), tam, descritorArquivo);
+   if(ret < tam) {
+      fprintf(stderr, "Erro de escrita no arquivo\n");
+      return 4;
+   }
+   //escreve os elementos da segunda matriz
+   ret = fwrite(matriz2, sizeof(float), tam, descritorArquivo);
+   if(ret < tam) {
+      fprintf(stderr, "Erro de escrita no arquivo\n");
+      return 4;
+   }
+
+   //obs: a qtde de colunas da primeira matriz deve ser igual à quantidade de linhas da segunda matriz.
+   #ifdef TEXTO
+   fprintf(stdout, "\n");
+   for(int i=0; i<m; i++) {
+      for(int j=0; j<n; j++)
+        fprintf(stdout, "%.6f ", matriz2[i*n+j]);
+      fprintf(stdout, "\n");
+   }
+   #endif
+
+   //finaliza o uso das variaveis
+   fclose(descritorArquivo);
+   free(matriz1);
+   free(matriz2);
+   return 0;
+}
+
---
+++ Lab3/sequencial.c
@@ -0,0 +1,138 @@
+/* Programa que le um arquivo binario com dois valores inteiros (indicando as dimensoes de uma matriz) 
+ * e uma sequencia com os valores da matriz (em float)
+ * Entrada: nome do arquivo de entrada
+ * Saida: valores da matriz escritos no formato texto (com 6 casas decimais) na saida padrao 
+ * */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+float* multiplicacaoSequencial(float *m1, float *m2, int linhas, int colunas){
+    int tam = linhas * colunas;
+    float *res = malloc(sizeof(float) * tam); // m1: n x m; m2: m x n; dimensao resultante: n x n
+    float soma;
+    int aux = 0; //para ajudar a escrever o elemento no vetor res
+
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < linhas; j++){
+            soma = 0;
+
+            for(int k = 0; k < colunas; k++){
+                soma += m1[i*colunas + k] * m2[k*linhas + j];
+                //printf("\nm1 = %f, m2 = %f\n", m1[i*colunas + k], m2[k*linhas + j]);
+            }
+
+            *(res+aux) = soma;
+            aux++;
+        }
+    }
+
+    return res;
+}
+
+int main(int argc, char*argv[]) {
+   float *matriz1, *matriz2, *resultado; //matrizes que serão carregadas do arquivo de entrada
+   //matriz 1: n x m
+   //matriz 2: m x n
+   //resultado: n x n
+   int n, m; //dimensoes da matriz
+   long long int tam; //qtde de elementos na matriz
+   FILE *arquivoEntrada, *arquivoSaida; //descritores do arquivo de entrada e de saida
+   size_t ret; //retorno da funcao de leitura no arquivo de entrada
+   
+   //recebe os argumentos de entrada
+   if(argc < 3) {
+      fprintf(stderr, "Digite: %s <arquivo entrada> <arquivo saida>\n", argv[0]);
+      return 1;
+   }
+   
+   
+   //abre o arquivo para leitura binaria
+   arquivoEntrada = fopen(argv[1], "rb");
+   if(!arquivoEntrada) {
+      fprintf(stderr, "Erro de abertura do arquivo\n");
+      return 2;
+   }
+
+   //le as dimensoes da matriz
+   ret = fread(&n, sizeof(int), 1, arquivoEntrada);
+   if(!ret) {
+      fprintf(stderr, "Erro de leitura das dimensoes da matriz arquivo \n");
+      return 3;
+   }
+   ret = fread(&m, sizeof(int), 1, arquivoEntrada);
+   if(!ret) {
+      fprintf(stderr, "Erro de leitura das dimensoes da matriz arquivo \n");
+      return 3;
+   }
+   tam = n * m; //calcula a qtde de elementos da matriz
+
+   //aloca memoria para a matriz 1
+   matriz1 = (float*) malloc(sizeof(float) * tam);
+   if(!matriz1) {
+      fprintf(stderr, "Erro de alocao da memoria da matriz 1\n");
+      return 3;
+   }
+
+   //carrega a matriz de elementos do tipo float do arquivo
+   ret = fread(matriz1, sizeof(float), tam, arquivoEntrada);
+   if(ret < tam) {
+      fprintf(stderr, "Erro de leitura dos elementos da matriz 1\n");
+      return 4;
+   }
+
+   //aloca memoria para a matriz 2
+   matriz2 = (float*) malloc(sizeof(float) * tam);
+   if(!matriz2) {
+      fprintf(stderr, "Erro de alocao da memoria da matriz 2\n");
+      return 3;
+   }
+
+   //carrega a matriz de elementos do tipo float do arquivo
+   ret = fread(matriz2, sizeof(float), tam, arquivoEntrada);
+   if(ret < tam) {
+      fprintf(stderr, "Erro de leitura dos elementos da matriz 2\n");
+      return 4;
+   }
+
+   /*imprime a matriz na saida padrao
+   for(int i=0; i<n; i++) { 
+      for(int j=0; j<m; j++)
+        fprintf(stdout, "%.6f ", matriz1[i*m+j]);
+      fprintf(stdout, "\n");
+   }*/
+
+    resultado = multiplicacaoSequencial(matriz1, matriz2, n, m);
+
+  //imprime a matriz na saida padrao
+   for(int i=0; i<n; i++) { 
+      for(int j=0; j<n; j++)
+        fprintf(stdout, "%.6f ", resultado[i*n+j]);
+      fprintf(stdout, "\n");
+   }
+
+   //abre o arquivo para escrita binaria
+   arquivoSaida = fopen(argv[2], "wb");
+   if(!arquivoSaida) {
+      fprintf(stderr, "Erro de abertura do arquivo\n");
+      return 2;
+   }
+   //escreve numero de n e de m
+   //ret = fwrite(&n, sizeof(int), 1, arquivoSaida);
+   //ret = fwrite(&m, sizeof(int), 1, arquivoSaida);
+   //escreve os elementos da matriz resultante
+   ret = fwrite(resultado, sizeof(float), tam, arquivoSaida);
+   if(ret < tam) {
+      fprintf(stderr, "Erro de escrita no arquivo\n");
+      return 4;
+   }
+
+   //finaliza o uso das variaveis
+   fclose(arquivoEntrada);
+   fclose(arquivoSaida);
+   free(matriz1);
+   free(matriz2);
+   return 0;
+}
+
