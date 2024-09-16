184979f5ac823336739d97e840ea339ea309b263
victorpss <victorpss@dcc.ufrj.br>
2024-09-16 19:34:52 -0300

    lab5 pronto

--- Lab5/soma-lock-atom.c
+++ Lab5/soma-lock-atom.c
@@ -1,60 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <pthread.h>
 
 long int soma = 0; //variavel compartilhada entre as threads
-int contador = 0; //variavel para contar a qtde de multiplos de 10 impressos
+int contador = 0; //variavel para contar a qtde de multiplos de 10 impresso
 pthread_mutex_t mutex; //variavel de lock para exclusao mutua
-pthread_cond_t cond_soma;
-pthread_cond_t cond_extra;
+pthread_cond_t cond_soma; //condicional para a thread de soma
+pthread_cond_t cond_extra; //condicional para a thread extra de print
 
-//funcao executada pelas threads
+//função das threads que incrementam soma
 void *ExecutaTarefa (void *arg) {
-  long int id = (long int) arg;
-  printf("Thread : %ld esta executando...\n", id);
+    long int id = (long int) arg;
+ 
+    printf("Thread : %ld esta executando...\n", id);
 
-  for (int i=0; i<100000; i++) {
-     //--entrada na SC
-     pthread_mutex_lock(&mutex);
-     //--SC (seção critica)
-     printf("soltei a thread extra");
-     pthread_cond_signal(&cond_extra);
-     if(!(soma % 10) && contador < 20){ // se cheguei a um multiplo de 10 e é um dos 20 primeiros
-      printf("cheguei aqui e prendi a thread %ld com contador = %d e soma = %ld\n", id, contador, soma);
-      pthread_cond_wait(&cond_soma, &mutex); // bloqueio para printar o valor de soma
-     } 
-     printf("thread %ld acabou de somar 1 à soma\n", id);
-     soma++; //incrementa a variavel compartilhada
-     //--saida da SC
-     pthread_mutex_unlock(&mutex);
-  }
+    for (int i=0; i<10000; i++) {
+        //--entrada na SC
+        pthread_mutex_lock(&mutex);
+        //--SC (seção critica)
+        while(soma == contador * 10 && contador < 20){ // se estou em um multiplo de 10 e é um dos primeiros
+            pthread_cond_signal(&cond_extra); // permite o print
+            pthread_cond_wait(&cond_soma, &mutex); // bloqueia para printar o valor de soma
+        }
+        soma++; //incrementa a variavel compartilhada (como ela está sendo incrementada após a checagem, o primeiro múltiplo checado é 0, e, na nossa definição, 0 é múltiplo de 10 e será printado)
+        //--saida da SC
+        pthread_mutex_unlock(&mutex);
+    }
   printf("Thread : %ld terminou!\n", id);
   pthread_exit(NULL);
 }
 
-//funcao executada pela thread de log
+//função da thread de print da soma
 void *extra (void *args) {
   printf("Extra : esta executando...\n");
-  for (int i=0; i<10000; i++) {
-    pthread_mutex_lock(&mutex);
-    
-    if (!(soma % 10)){ //imprime se 'soma' for multiplo de 10
-      printf("soma = %ld, i = %d \n", soma, i); //não impede de printar o mesmo número repetidamente, porque a thread da soma pode entrar no processador diversas vezes antes da thread de incrementar a soma.
+ 
+  for(int i=0; i<10000 && contador < 20; i++){
+    pthread_mutex_lock(&mutex); // entrada na seção crítica
+   
+    if (!(soma%10) && soma == contador * 10){ // imprime se 'soma' for multiplo de 10 e é igual ao contador atual multiplicado por 10 (garante print unico do valor da soma)
+      printf("soma = %ld \n", soma);
       contador++;
-
-
-      printf("soltei a thread soma\n");
-      pthread_cond_broadcast(&cond_soma); // libero as threads que estão somando
-
     }
-      if(contador < 20){
-        printf("prendi a thread extra\n");
-        pthread_cond_wait(&cond_extra, NULL);
-      }
-    pthread_mutex_unlock(&mutex);
+    else{ // se nao é multiplo ou se ele já foi printado, a extra é bloqueada ate chegar algum novo multiplo (e as threads de soma serão desbloqueadas)
+       pthread_cond_broadcast(&cond_soma);
+       pthread_cond_wait(&cond_extra, &mutex);
+    }
+
+    pthread_mutex_unlock(&mutex); // saída da seção crítica
   }
-  printf("Extra : terminou!\n");
-  pthread_exit(NULL);
+    pthread_cond_broadcast(&cond_soma); // para garantir que nenhuma thread ficará bloqueada
+    printf("Extra : terminou!\n");
+    pthread_exit(NULL);
 }
 
 //fluxo principal
@@ -62,7 +58,7 @@ int main(int argc, char *argv[]) {
    pthread_t *tid; //identificadores das threads no sistema
    int nthreads; //qtde de threads (passada linha de comando)
 
-   //--inicilaiza o mutex (lock de exclusao mutua) e a variavel de condicao
+   //--inicilaiza o mutex (lock de exclusao mutua) e as variaveis de condicao
    pthread_mutex_init(&mutex, NULL);
    pthread_cond_init(&cond_soma, NULL);
    pthread_cond_init(&cond_extra, NULL);
@@ -93,11 +89,11 @@ int main(int argc, char *argv[]) {
    //--espera todas as threads terminarem
    for (int t=0; t<nthreads+1; t++) {
      if (pthread_join(tid[t], NULL)) {
-         printf("--ERRO: pthread_join() \n"); exit(-1); 
-     } 
-   } 
+         printf("--ERRO: pthread_join() \n"); exit(-1);
+     }
+   }
 
-   //--finaliza o mutex e o cond
+   //--finaliza o mutex e as condições
    pthread_mutex_destroy(&mutex);
    pthread_cond_destroy(&cond_soma);
    pthread_cond_destroy(&cond_extra);
